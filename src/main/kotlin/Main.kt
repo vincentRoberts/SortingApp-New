f30fc79b88a99118fc577cea3eea1f7d7375b919
vincentRoberts <20103510@mail.wit.ie>
2024-08-15 13:33:37 +0100

    Update in menu: Creation of SearchScreen.kt, and SortScreen.kt, and the introduction of a tabbed menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,51 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ui.SearchScreen
+import ui.SortScreen
 import ui.theme.AppTheme
 
 @Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(currentScreen: MutableState<MenuItem>) {
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    Column {
+        TabRow(selectedTabIndex = currentScreen.value.ordinal) {
+
+            Tab(selected = currentScreen.value == MenuItem.Search,
+                onClick = { currentScreen.value = MenuItem.Search }
+            ){
+                Text(text = "Search")
+            }
+
+            Tab(selected = currentScreen.value == MenuItem.Sort,
+                onClick = { currentScreen.value = MenuItem.Sort }
+            ){
+                Text(text = "Sort")
+            }
+        }
+
+        Box {
+            when (currentScreen.value) {
+                MenuItem.Search -> SearchScreen()
+                MenuItem.Sort -> SortScreen()
+            }
         }
     }
 }
 
 fun main() = application {
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Custom Data Structures"
+        title = "Searching and Sorting App"
     ) {
-        AppTheme {
-            App()
+        AppTheme() {
+            val currentScreen = remember { mutableStateOf(MenuItem.Search) }
+            App(currentScreen)
         }
     }
 }
